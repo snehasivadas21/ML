89bc9fb932bf728ff3782f8d91d017c319faae6d
snehasivadas21 <snehasivadas2002@gmail.com>
2025-11-04 21:53:11 +0530

    more exercise

--- numpy1.py
+++ numpy1.py
@@ -18,11 +18,11 @@ import numpy as np
 # arr2=arr[:2,::2]
 # print(arr2)
 
-# a1=np.array([[1,2],
-#             [3,4]])
+a1=np.array([[1,2],
+            [3,4]])
 
-# b1=np.array([[4,3],
-#              [2,1]])
+b1=np.array([[4,3],
+             [2,1]])
 # print(a1+1)
 # print(b1-2)
 # print(a1.sum())
@@ -30,6 +30,7 @@ import numpy as np
 # print(a1.max())
 # print(a1.min())
 # print(a1.mean())
+print(np.vstack((a1,b1)))
 
 # x=np.array([1,2])
 # y=np.array([1.0,2,2])
@@ -43,10 +44,37 @@ import numpy as np
 # c=np.array([1,2,3,4,5,6])
 # print(c.reshape(3,2))
 
-d=np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(d)
-print(d.sum())
-print(d.mean())
-print(d.sum(axis=0))
-print(d.sum(axis=1))
-print(d.T)
+# d=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# print(d)
+# print(d.sum())
+# print(d.mean())
+# print(d.sum(axis=0))
+# print(d.sum(axis=1))
+# print(d.T)
+
+# r=np.random.random(size=(2,3))
+# print(r)
+
+ran=np.random.rand(10)
+uni=np.unique(ran)
+print(ran)
+print(uni)
+
+# d=np.array([10,220,30,400,50,6,70])
+# print(format(d))
+# i=np.where(d==30)
+# print(format(i[0]))
+# d.sort()
+# print(d)
+
+# a=np.array([[12,15],[10,1]])
+# a1=np.sort(a,axis=0)
+# a2=np.sort(a,axis=-1)
+# a3=np.sort(a,axis=None)
+# print(a1)
+# print(a2)
+# print(a3)
+
+a1=np.array([1,2,3])
+a2=np.array([1,5,3])
+print(np.dot(a1,a2))
--- pandas1.py
+++ pandas1.py
@@ -1,8 +1,88 @@
 import pandas as pd
 import numpy as np
 
-s=pd.Series()
-print(s)
-data=np.array(['s','n','e','h','a'])
-s=pd.Series(data)
-print(s)
+# s=pd.Series()
+# print(s)
+
+# data=np.array(['s','n','e','h','a'])
+# s=pd.Series(data)
+# r=pd.Series(data,index=[11,22,33,44,55])
+# print(s)
+# print(s[:2])
+# print(r[33])
+
+# d={"a":10,"b":20,"c":30}
+# ds=pd.Series(d)
+# print(ds)
+
+# r=pd.Series(range(5,15))
+# print(r)
+
+# l=pd.Series(range(1,20,3),index=[x for x in 'abcdefg'])
+# print(l)
+
+
+
+# d=pd.DataFrame()
+# print(d)
+
+# l=["im","a","develper"]
+# d=pd.DataFrame(l)
+# print(d)
+
+# d=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# df=pd.DataFrame(d,columns=['A','B','C'])
+# print(df)
+
+di={'name'  :["a","b","c","d"],
+    'degree':["BA","BCA","BBA","BCom"],
+    'score' :[90,80,40,60],
+    'age'   :[1,2,3,4]}
+df=pd.DataFrame(di)
+print(df)
+# print(df[['name','score']])
+# print(df[(df['score']>70) & (df['degree']=='BA')])
+# print(pd.concat([df.head(2)]))
+# print(pd.concat([df.tail(2)]))
+# df['double_score']=np.multiply(df['score'],2)
+# print(df)
+# df['sqrt']=np.sqrt(df['age'])
+# print(df)
+sort=df.sort_values('score')
+print(sort)
+
+# d1={'ID':[1,2,3],'Name':["cat","dog","rat"]}
+# d2={'ID':[2,3,4],'Age':[23,24,25]}
+# d11=pd.DataFrame(d1)
+# d22=pd.DataFrame(d2)
+# print(pd.merge(d11,d22,on='ID'))
+
+# d={'fscore':[10,20,np.nan,30],
+#    'sscore':[40,50,60,np.nan],
+#    'tscore':[np.nan,70,80,90],
+#    'fscore':[100,200,300,400]}
+# df=pd.DataFrame(d)
+# print(df.isnull())
+# print(df.fillna(df.mean()))
+# print(df.dropna())
+# print(df)
+
+# df=pd.read_csv("people_data.csv")
+# print(df)
+
+# df=pd.read_csv("people_data.csv",usecols=["First Name","Email"])
+# print(df)
+
+# df=pd.read_csv("people_data.csv",index_col="First Name")
+# print(df)
+
+# df=pd.read_csv('people_data.csv',nrows=3)
+# print(df)
+
+data = {'Category': ['A', 'B', 'A', 'B', 'A', 'B'],
+        'Values': [100, 200, 300, 400, 500, 600]}
+df=pd.DataFrame(data)
+mean=df.groupby('Category')['Values'].mean()
+print(mean)
+print(df.rename(columns={'Category':'Cat','Values':'val'}))
+print(df.transpose())
